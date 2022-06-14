7eff1ac886c19830dc6534eca956ed881d818ba9
leaurh <arinamehrengina@yandex.ru>
2022-06-14 20:13:41 +0300

    solution commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,9 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
